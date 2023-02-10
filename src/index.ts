0695b289bb2352d1fcb30b2d80c56803341069e4
Digambar <deshawaldigambar@gmail.com>
2023-02-10 20:00:47 +0530

    added project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,85 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuid } from 'uuid';
 
-import confetti from 'canvas-confetti';
+let userInput = document.querySelector<HTMLInputElement>('#task-title');
+let addBtn = document.querySelector<HTMLButtonElement>('#add-btn');
+let listOfTask = document.querySelector<HTMLUListElement>('#lists');
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+console.log(addBtn);
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+let allTasks: Task[] = getTasks();
+allTasks.forEach(addTask);
+
+addBtn?.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (userInput?.value == '' || userInput?.value == null) return;
+
+  const newTask = {
+    id: uuid(),
+    title: userInput.value,
+    completed: false,
+  };
+
+  allTasks.push(newTask);
+
+  addTask(newTask);
+  userInput.value = '';
+});
+
+function addTask(task: Task) {
+  let item = document.createElement('li');
+  let label = document.createElement('label');
+  let input = document.createElement('input');
+//   let deleteBtn = document.createElement('button');
+//   deleteBtn.innerText="delete"
+
+//   deleteBtn.addEventListener("click" ,(e)=>{
+// 	e.preventDefault();
+// 	deleteTask(task)
+// 	console.log(allTasks);
+//   })
+   
+  input.addEventListener('change', (e) => {
+    task.completed = input.checked;
+    if (task.completed) {
+      label.style.textDecoration = 'line-through';
+    } else {
+      label.style.textDecoration = 'none';
+    }
+    saveTasks();
+  });
+
+  if (task.completed) {
+    label.style.textDecoration = 'line-through';
+  } else {
+    label.style.textDecoration = 'none';
+  }
+
+  input.type = 'checkbox';
+  input.checked = task.completed;
+  label.append(input, task.title);
+
+  item.append(label);
+  listOfTask?.append(item);
+  console.log('added');
+}
+
+function saveTasks() {
+  localStorage.setItem('allTasks', JSON.stringify(allTasks));
+}
+
+function getTasks(): Task[] {
+  let jsonTask = localStorage.getItem('allTasks');
+  if (jsonTask === null) return [];
+  return JSON.parse(jsonTask);
+}
+
+// function deleteTask(task:Task){
+// 	return allTasks.filter(item=>item.id!==task.id);
+// }
